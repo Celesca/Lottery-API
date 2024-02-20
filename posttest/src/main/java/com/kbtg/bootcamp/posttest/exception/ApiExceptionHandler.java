30dad415bfc897009574c39619419b2b8459b0fa
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-02-21 00:39:46 +0700

    ADD: ExceptionHandler and Repository

--- posttest/src/main/java/com/kbtg/bootcamp/posttest/exception/ApiExceptionHandler.java
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/exception/ApiExceptionHandler.java
@@ -15,18 +15,43 @@ import java.time.ZonedDateTime;
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
-//    @ExceptionHandler(value = {NotFoundException.class})
-//    // Exception ถ้าเป็นคลาสนี้เเราจะใช้ Function แล้วรับเข้ามา แล้ว Build Response Error รํูปแบบใหม่
-//    public ResponseEntity<Object> handleNotFoundException(NotFoundException notFoundException) {
-//
-//        ApiExceptionResponse apiExceptionResponse = new ApiExceptionResponse(
-//                notFoundException.getMessage(),
-//                HttpStatus.NOT_FOUND,
-//                ZonedDateTime.now()
-//        );
-//
-//        return new ResponseEntity<>(apiExceptionResponse, HttpStatus.BAD_REQUEST);
-//    }
+
+    // Bad Request Exception
+    @ExceptionHandler(value = {BadRequestException.class})
+    public ResponseEntity<Object> handleBadRequestException(BadRequestException badRequestException) {
+        ApiExceptionResponse apiExceptionResponse = new ApiExceptionResponse(
+                badRequestException.getMessage(),
+                HttpStatus.BAD_REQUEST,
+                ZonedDateTime.now()
+        );
+        return new ResponseEntity<>(apiExceptionResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    // Internal Service Exception
+    @ExceptionHandler(value = {InternalServiceException.class})
+    public ResponseEntity<Object> handleInternalServiceException(InternalServiceException internalServiceException) {
+        ApiExceptionResponse apiExceptionResponse = new ApiExceptionResponse(
+                internalServiceException.getMessage(),
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                ZonedDateTime.now()
+        );
+        return new ResponseEntity<>(apiExceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    // Not Found Exception
+    @ExceptionHandler(value = {NotFoundException.class})
+    public ResponseEntity<Object> handleNotFoundException(NotFoundException notFoundException) {
+        ApiExceptionResponse apiExceptionResponse = new ApiExceptionResponse(
+                notFoundException.getMessage(),
+                HttpStatus.NOT_FOUND,
+                ZonedDateTime.now()
+        );
+        return new ResponseEntity<>(apiExceptionResponse, HttpStatus.NOT_FOUND);
+    }
+
+
+
+
 
 
 
---
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/repository/Lottery.java
@@ -0,0 +1,24 @@
+package com.kbtg.bootcamp.posttest.repository;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+@Entity
+@Table(name = "lottery")
+public class Lottery {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer ticket;
+
+    @NotNull
+    @Positive
+    private Integer price;
+
+    @NotNull
+    @Positive
+    private Integer amount;
+
+
+}
